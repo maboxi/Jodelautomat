53b855c803adcf38d15ac18ef157b9e04c74f771
Maximilian Pöllinger <maxi.poellinger@gmail.com>
2023-05-02 01:16:25 +0200

    plotting fft data with recursive running max

--- JAVS-FFTVisualizer/visualizer.py
+++ JAVS-FFTVisualizer/visualizer.py
@@ -6,7 +6,7 @@ from math import sin, atan2
 
 import struct
 
-#SERIALCOM_USED = 'CDC'
+#SERIALCOM_USED = 'CDC' # not supported right now!
 SERIALCOM_USED = 'UART'
 
 SERIALPORT_CDC = 'COM7'
@@ -23,10 +23,17 @@ UART_NUMSAMPLES_HALF = int(UART_NUMSAMPLES / 2)
 FFT_NUMSAMPLES = 4096
 FFT_NUMCYCLES = 33
 
+
 serialHandle: serial.Serial = None
-axPower = None
-axPhase = None
+axTop = None
+axBot = None
 fftMaxFrequency = FFT_NUMSAMPLES * FFT_NUMCYCLES / 2 # 35 sampling cycles per second, 4096 samples / cycle
+
+FFT_NUMRECURSIVEMAX = 60
+fftRecursiveMaxValues = [0] * 60
+fftRecursiveMaxCounter = 0
+
+
 
 def serialWaitFor(s, name):
     global serialHandle
@@ -39,48 +46,19 @@ def serialWaitFor(s, name):
         else:
             print(f"Received: '{line}'")
             pass
-
+    
 def animatePlotCDC(i):
-    global serialHandle
-    
-    wait = input("waiting... press enter")
-    
-    serialHandle.write(b"sendfft\r\n")
-    serialHandle.flush()
-    print("Waiting for CDC data... ")
-    
-    serialWaitFor(CDC_FFTSTART, "preamble")
-    
-    receivedData = serialHandle.readline()
-    dataStringArray = receivedData.split(b' ')
-    dataStringArray = dataStringArray[:(len(dataStringArray) - 1)]
-    
-    serialWaitFor(CDC_FFTEND, "epilogue")
-    
-    print(f"Received {len(receivedData)} bytes in var of type {type(receivedData)}")
-    
-    
-    dataArray = [float(x) for x in dataStringArray]
-    
-    fftFrequencyPerBin = fftMaxFrequency / len(dataArray)
-    
-    plotXValues = [fftFrequencyPerBin * x for x in range(0, len(dataArray))]
-    #plotYValues = [y if y > 0.0 else -y for y in dataArray]
-    plotYValues = dataArray
-    
-    ax.clear()
-    ax.set_xlim(0, fftMaxFrequency)
-    ax.plot(plotXValues, plotYValues)
-    
+    pass
 
 def animatePlotUART(i):
     global serialHandle
-    global axPower
-    global axPhase
-    global ymax
+    global axTop
+    global axBot
     global fftMaxFrequency
+    global fftRecursiveMaxValues
+    global fftRecursiveMaxCounter
     
-    print("Waiting for UART data... ")
+    print("\nWaiting for UART data... ")
     
     receivedData = None
     
@@ -100,27 +78,42 @@ def animatePlotUART(i):
     print(f"Received {len(receivedData)} bytes and converted them into {len(dataArray)} numbers")
     
     fftFrequencyPerBin = fftMaxFrequency / (FFT_NUMSAMPLES / 2)
+
     
-    #(a,) = dataArray[0]
-    #print(a)
-    
+    # calculate fft results from complex result data
     realFFT = [dataArray[2*i] for i in range(0, UART_NUMSAMPLES_HALF)]
     imaginaryFFT = [dataArray[2*i + 1] for i in range(0, UART_NUMSAMPLES_HALF)]
     angleFFT = [atan2(imaginaryFFT[i], realFFT[i]) for i in range(0, UART_NUMSAMPLES_HALF)]
     powerFFT = [realFFT[i] * realFFT[i] + imaginaryFFT[i] * imaginaryFFT[i] for i in range(0, UART_NUMSAMPLES_HALF)]
     
+    powerMax = max(powerFFT)
+
+    fftRecursiveMaxValues[fftRecursiveMaxCounter] = powerMax * 1.1
+    fftRecursiveMaxValuesSloped = fftRecursiveMaxValues
     
+    slope = 1 / (16 * FFT_NUMRECURSIVEMAX)
+    
+    for i in range(0, FFT_NUMRECURSIVEMAX):
+        j =  (i + fftRecursiveMaxCounter) % FFT_NUMRECURSIVEMAX
+        fftRecursiveMaxValuesSloped[j] = fftRecursiveMaxValuesSloped[j] * (1 - i * slope)
+        
+        
+    fftRecursiveMaxCounter = (fftRecursiveMaxCounter + 1) % FFT_NUMRECURSIVEMAX
+    
+    fftRecursiveMax = max(fftRecursiveMaxValuesSloped)
+    
+    print(fftRecursiveMax)
+
     plotXValues = [fftFrequencyPerBin * x for x in range(0, UART_NUMSAMPLES_HALF)]
     
-    (powMin, powMax) = axPower.get_ylim()
-    powMin = min(powMin, min(powerFFT))
-    powMax = max(powMax, max(powerFFT))
+
     
-    axPower.clear()
-    axPower.set_xlim(0, fftFrequencyPerBin * (UART_NUMSAMPLES_HALF + 1))
-    axPower.plot(plotXValues, powerFFT)
-    axPower.set_ylim(powMin, powMax)
+    axTop.clear()
+    axTop.set_xlim(0, fftFrequencyPerBin * (UART_NUMSAMPLES_HALF + 1))
+    axTop.plot(plotXValues, powerFFT)
+    axTop.set_ylim(0, fftRecursiveMax)
     
+    '''
     (phaMin, phaMax) = axPhase.get_ylim()
     phaMin = min(phaMin, min(angleFFT))
     phaMax = max(phaMax, max(angleFFT))
@@ -129,7 +122,15 @@ def animatePlotUART(i):
     axPhase.set_xlim(0, fftFrequencyPerBin * (UART_NUMSAMPLES_HALF + 1))
     axPhase.plot(plotXValues, angleFFT)
     axPhase.set_ylim(phaMin, phaMax)
+    '''
     
+    (_, powMax) = axBot.get_ylim()
+    powMax = max(powMax, powerMax)
+    
+    axBot.clear()
+    axBot.set_xlim(0, fftFrequencyPerBin * (UART_NUMSAMPLES_HALF + 1))
+    axBot.plot(plotXValues, powerFFT)
+    axBot.set_ylim(0, powMax)
 
 
 if __name__ == '__main__':
@@ -138,31 +139,30 @@ if __name__ == '__main__':
     elif(SERIALCOM_USED == 'CDC'):
         serialHandle  = serial.Serial(port=SERIALPORT_CDC)
     else:
-        print("Unknown serial port selection (var SERIALCOM_USED)!")
+        print(f"Unknown serial port selection (var SERIALCOM_USED = {SERIALCOM_USED})!")
         exit(1)
     
     serialHandle.set_buffer_size(65535, 128)
-    #serialWrapper = io.TextIOWrapper(io.BufferedRWPair(serialHandle, serialHandle), newline='\r\n' )
     
     print("Serial connection established!")
     
     fig1 = plt.figure(1)
     
-    axPower = fig1.add_subplot(2, 1, 1)
-    axPhase = fig1.add_subplot(2, 1, 2)
+    axTop = fig1.add_subplot(2, 1, 1)
+    axBot = fig1.add_subplot(2, 1, 2)
     
-    axPower.plot([], [])
-    axPhase.plot([], [])
+    axTop.plot([], [])
+    axBot.plot([], [])
     
     
     if(SERIALCOM_USED == 'UART'):
         fftFrequencyPerBin = fftMaxFrequency / FFT_NUMSAMPLES
         
-        axPower.set_xlim(0, fftFrequencyPerBin * UART_NUMSAMPLES)
-        axPhase.set_xlim(0, fftFrequencyPerBin * UART_NUMSAMPLES)
+        axTop.set_xlim(0, fftFrequencyPerBin * UART_NUMSAMPLES)
+        axBot.set_xlim(0, fftFrequencyPerBin * UART_NUMSAMPLES)
         
-        axPower.set_ylim(-1.0, 1.0)
-        axPhase.set_ylim(-1.0, 1.0)
+        axTop.set_ylim(-1.0, 1.0)
+        axBot.set_ylim(-1.0, 1.0)
         
         ani = animation.FuncAnimation(fig1, animatePlotUART, interval=1) # max 50 fps
     elif(SERIALCOM_USED == 'CDC'):
